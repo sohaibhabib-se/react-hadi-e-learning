8659508020d10b1de9a4a124820e1c6d813a7798
Sohaib Habib <sohaibhabib8@gmail.com>
2024-01-22 17:05:31 +0500

    API calling and custom hooks

--- project1/frontend/src/App.jsx
+++ project1/frontend/src/App.jsx
@@ -1,34 +1,18 @@
-import axios from 'axios'
-import { useState } from 'react'
+import { useEffect } from 'react';
+import { _useCategories } from './logic/actions/_categories';
 
 const App = () => {
-  const [categories, setCategories] = useState([]);
-  const [loading, setLoading] = useState(false);
+  
 
-  const getCategories = async () => {
-    setLoading(true)
-    try {
-      const res = await axios.get('http://localhost:8000/category')
-      console.log(res)
-      setCategories(res.data.categories)
-    } catch (error) {
-      console.log(error)
+  const {loading, /*getCategories,*/ categories} = _useCategories();
 
-      // if (error.response.data.status === 401) {
-      // } else if (error.response.data.status === 500) {
-      // } else if (error.response.data.status === 403) {
-      // } else if (error.response.data.status === 404) {
-      // }
-
-      alert('Sorry, Failed try again')
-    } finally {
-      setLoading(false)
-    }
-  }
+  // useEffect(() => {
+  //   getCategories();
+  // }, []);
 
   return (
     <div>
-      <button onClick={getCategories}>Get Data</button>
+      {/* <button onClick={getCategories}>Get Data</button> */}
       <br />
       Data: {loading ? "Please wait..." : JSON.stringify(categories)}
     </div>
--- project1/frontend/src/logic/actions/_categories.js
+++ project1/frontend/src/logic/actions/_categories.js
@@ -1,7 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const _useCategories = () => {
     
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
 
 
 
-  return {};
+  const getCategories = async () => {
+    setLoading(true)
+    try {
+      const res = await axios.get('http://localhost:8000/category')
+      console.log(res)
+      setCategories(res.data.categories)
+    } catch (error) {
+      console.log(error)
+
+      // if (error.response.data.status === 401) {
+      // } else if (error.response.data.status === 500) {
+      // } else if (error.response.data.status === 403) {
+      // } else if (error.response.data.status === 404) {
+      // }
+
+      alert('Sorry, Failed try again')
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getCategories();
+  }, []);
+
+
+  return {loading, /*getCategories,*/ categories};
 }
